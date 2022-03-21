78a8e6d4959b2877d202d93be4c3a9b6680495e7
herobrinenrj@gmail.com <herobrinenrj@gmail.com>
2022-03-22 01:29:32 +0500

    Add project files.

---
+++ MSSQL-Tools/MainForm.Designer.cs
@@ -0,0 +1,410 @@
+﻿namespace MSSQL_Tools
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_c1 = new System.Windows.Forms.TextBox();
+            this.tb_c2 = new System.Windows.Forms.TextBox();
+            this.tb_c3 = new System.Windows.Forms.TextBox();
+            this.tb_c4 = new System.Windows.Forms.TextBox();
+            this.tb_c7 = new System.Windows.Forms.TextBox();
+            this.tb_c6 = new System.Windows.Forms.TextBox();
+            this.tb_c5 = new System.Windows.Forms.TextBox();
+            this.tb_c8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_c1 = new System.Windows.Forms.ComboBox();
+            this.cb_c2 = new System.Windows.Forms.ComboBox();
+            this.cb_c3 = new System.Windows.Forms.ComboBox();
+            this.cb_c4 = new System.Windows.Forms.ComboBox();
+            this.cb_c8 = new System.Windows.Forms.ComboBox();
+            this.cb_c7 = new System.Windows.Forms.ComboBox();
+            this.cb_c6 = new System.Windows.Forms.ComboBox();
+            this.cb_c5 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.B_GenRandomInsert = new System.Windows.Forms.Button();
+            this.rtb_out = new System.Windows.Forms.RichTextBox();
+            this.tb_Count = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_TableName = new System.Windows.Forms.TextBox();
+            this.B_Copy = new System.Windows.Forms.Button();
+            this.B_Clear = new System.Windows.Forms.Button();
+            this.B_InsertIntoTable = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_Database = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_Server = new System.Windows.Forms.TextBox();
+            this.B_CurrencyRate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_c1
+            // 
+            this.tb_c1.Location = new System.Drawing.Point(70, 12);
+            this.tb_c1.Name = "tb_c1";
+            this.tb_c1.Size = new System.Drawing.Size(56, 23);
+            this.tb_c1.TabIndex = 0;
+            // 
+            // tb_c2
+            // 
+            this.tb_c2.Location = new System.Drawing.Point(132, 12);
+            this.tb_c2.Name = "tb_c2";
+            this.tb_c2.Size = new System.Drawing.Size(56, 23);
+            this.tb_c2.TabIndex = 1;
+            // 
+            // tb_c3
+            // 
+            this.tb_c3.Location = new System.Drawing.Point(194, 12);
+            this.tb_c3.Name = "tb_c3";
+            this.tb_c3.Size = new System.Drawing.Size(56, 23);
+            this.tb_c3.TabIndex = 2;
+            // 
+            // tb_c4
+            // 
+            this.tb_c4.Location = new System.Drawing.Point(256, 12);
+            this.tb_c4.Name = "tb_c4";
+            this.tb_c4.Size = new System.Drawing.Size(56, 23);
+            this.tb_c4.TabIndex = 3;
+            // 
+            // tb_c7
+            // 
+            this.tb_c7.Location = new System.Drawing.Point(442, 12);
+            this.tb_c7.Name = "tb_c7";
+            this.tb_c7.Size = new System.Drawing.Size(56, 23);
+            this.tb_c7.TabIndex = 6;
+            // 
+            // tb_c6
+            // 
+            this.tb_c6.Location = new System.Drawing.Point(380, 12);
+            this.tb_c6.Name = "tb_c6";
+            this.tb_c6.Size = new System.Drawing.Size(56, 23);
+            this.tb_c6.TabIndex = 5;
+            // 
+            // tb_c5
+            // 
+            this.tb_c5.Location = new System.Drawing.Point(318, 12);
+            this.tb_c5.Name = "tb_c5";
+            this.tb_c5.Size = new System.Drawing.Size(56, 23);
+            this.tb_c5.TabIndex = 4;
+            // 
+            // tb_c8
+            // 
+            this.tb_c8.Location = new System.Drawing.Point(504, 12);
+            this.tb_c8.Name = "tb_c8";
+            this.tb_c8.Size = new System.Drawing.Size(56, 23);
+            this.tb_c8.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Columns";
+            // 
+            // cb_c1
+            // 
+            this.cb_c1.FormattingEnabled = true;
+            this.cb_c1.Location = new System.Drawing.Point(70, 41);
+            this.cb_c1.Name = "cb_c1";
+            this.cb_c1.Size = new System.Drawing.Size(56, 23);
+            this.cb_c1.TabIndex = 9;
+            // 
+            // cb_c2
+            // 
+            this.cb_c2.FormattingEnabled = true;
+            this.cb_c2.Location = new System.Drawing.Point(132, 41);
+            this.cb_c2.Name = "cb_c2";
+            this.cb_c2.Size = new System.Drawing.Size(56, 23);
+            this.cb_c2.TabIndex = 10;
+            // 
+            // cb_c3
+            // 
+            this.cb_c3.FormattingEnabled = true;
+            this.cb_c3.Location = new System.Drawing.Point(194, 41);
+            this.cb_c3.Name = "cb_c3";
+            this.cb_c3.Size = new System.Drawing.Size(56, 23);
+            this.cb_c3.TabIndex = 11;
+            // 
+            // cb_c4
+            // 
+            this.cb_c4.FormattingEnabled = true;
+            this.cb_c4.Location = new System.Drawing.Point(256, 41);
+            this.cb_c4.Name = "cb_c4";
+            this.cb_c4.Size = new System.Drawing.Size(56, 23);
+            this.cb_c4.TabIndex = 12;
+            // 
+            // cb_c8
+            // 
+            this.cb_c8.FormattingEnabled = true;
+            this.cb_c8.Location = new System.Drawing.Point(504, 41);
+            this.cb_c8.Name = "cb_c8";
+            this.cb_c8.Size = new System.Drawing.Size(56, 23);
+            this.cb_c8.TabIndex = 16;
+            // 
+            // cb_c7
+            // 
+            this.cb_c7.FormattingEnabled = true;
+            this.cb_c7.Location = new System.Drawing.Point(442, 41);
+            this.cb_c7.Name = "cb_c7";
+            this.cb_c7.Size = new System.Drawing.Size(56, 23);
+            this.cb_c7.TabIndex = 15;
+            // 
+            // cb_c6
+            // 
+            this.cb_c6.FormattingEnabled = true;
+            this.cb_c6.Location = new System.Drawing.Point(380, 41);
+            this.cb_c6.Name = "cb_c6";
+            this.cb_c6.Size = new System.Drawing.Size(56, 23);
+            this.cb_c6.TabIndex = 14;
+            // 
+            // cb_c5
+            // 
+            this.cb_c5.FormattingEnabled = true;
+            this.cb_c5.Location = new System.Drawing.Point(318, 41);
+            this.cb_c5.Name = "cb_c5";
+            this.cb_c5.Size = new System.Drawing.Size(56, 23);
+            this.cb_c5.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Type";
+            // 
+            // B_GenRandomInsert
+            // 
+            this.B_GenRandomInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.B_GenRandomInsert.Location = new System.Drawing.Point(194, 294);
+            this.B_GenRandomInsert.Name = "B_GenRandomInsert";
+            this.B_GenRandomInsert.Size = new System.Drawing.Size(180, 32);
+            this.B_GenRandomInsert.TabIndex = 18;
+            this.B_GenRandomInsert.Text = "Generate Random";
+            this.B_GenRandomInsert.UseVisualStyleBackColor = true;
+            this.B_GenRandomInsert.Click += new System.EventHandler(this.B_GenRandomInsert_Click);
+            // 
+            // rtb_out
+            // 
+            this.rtb_out.Location = new System.Drawing.Point(12, 145);
+            this.rtb_out.Name = "rtb_out";
+            this.rtb_out.Size = new System.Drawing.Size(560, 141);
+            this.rtb_out.TabIndex = 19;
+            this.rtb_out.Text = "";
+            // 
+            // tb_Count
+            // 
+            this.tb_Count.Location = new System.Drawing.Point(70, 76);
+            this.tb_Count.Name = "tb_Count";
+            this.tb_Count.Size = new System.Drawing.Size(56, 23);
+            this.tb_Count.TabIndex = 20;
+            this.tb_Count.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(307, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 15);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Table";
+            // 
+            // tb_TableName
+            // 
+            this.tb_TableName.Location = new System.Drawing.Point(347, 76);
+            this.tb_TableName.Name = "tb_TableName";
+            this.tb_TableName.Size = new System.Drawing.Size(89, 23);
+            this.tb_TableName.TabIndex = 22;
+            this.tb_TableName.Text = "TestTable";
+            // 
+            // B_Copy
+            // 
+            this.B_Copy.Location = new System.Drawing.Point(416, 299);
+            this.B_Copy.Name = "B_Copy";
+            this.B_Copy.Size = new System.Drawing.Size(75, 23);
+            this.B_Copy.TabIndex = 24;
+            this.B_Copy.Text = "Copy";
+            this.B_Copy.UseVisualStyleBackColor = true;
+            this.B_Copy.Click += new System.EventHandler(this.B_Copy_Click);
+            // 
+            // B_Clear
+            // 
+            this.B_Clear.Location = new System.Drawing.Point(497, 299);
+            this.B_Clear.Name = "B_Clear";
+            this.B_Clear.Size = new System.Drawing.Size(75, 23);
+            this.B_Clear.TabIndex = 25;
+            this.B_Clear.Text = "Clear";
+            this.B_Clear.UseVisualStyleBackColor = true;
+            this.B_Clear.Click += new System.EventHandler(this.B_Clear_Click);
+            // 
+            // B_InsertIntoTable
+            // 
+            this.B_InsertIntoTable.Enabled = false;
+            this.B_InsertIntoTable.Location = new System.Drawing.Point(12, 299);
+            this.B_InsertIntoTable.Name = "B_InsertIntoTable";
+            this.B_InsertIntoTable.Size = new System.Drawing.Size(118, 23);
+            this.B_InsertIntoTable.TabIndex = 26;
+            this.B_InsertIntoTable.Text = "Insert into Table";
+            this.B_InsertIntoTable.UseVisualStyleBackColor = true;
+            this.B_InsertIntoTable.Click += new System.EventHandler(this.B_InsertIntoTable_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(133, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Database";
+            // 
+            // tb_Database
+            // 
+            this.tb_Database.Location = new System.Drawing.Point(194, 108);
+            this.tb_Database.Name = "tb_Database";
+            this.tb_Database.Size = new System.Drawing.Size(107, 23);
+            this.tb_Database.TabIndex = 27;
+            this.tb_Database.Text = "TestDB";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(149, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 15);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Server";
+            // 
+            // tb_Server
+            // 
+            this.tb_Server.Location = new System.Drawing.Point(194, 76);
+            this.tb_Server.Name = "tb_Server";
+            this.tb_Server.Size = new System.Drawing.Size(107, 23);
+            this.tb_Server.TabIndex = 29;
+            this.tb_Server.Text = ".\\SQLEXPRESS";
+            // 
+            // B_CurrencyRate
+            // 
+            this.B_CurrencyRate.Location = new System.Drawing.Point(504, 116);
+            this.B_CurrencyRate.Name = "B_CurrencyRate";
+            this.B_CurrencyRate.Size = new System.Drawing.Size(56, 23);
+            this.B_CurrencyRate.TabIndex = 31;
+            this.B_CurrencyRate.Text = "Курс";
+            this.B_CurrencyRate.UseVisualStyleBackColor = true;
+            this.B_CurrencyRate.Click += new System.EventHandler(this.B_CurrencyRate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 333);
+            this.Controls.Add(this.B_CurrencyRate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tb_Server);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_Database);
+            this.Controls.Add(this.B_InsertIntoTable);
+            this.Controls.Add(this.B_Clear);
+            this.Controls.Add(this.B_Copy);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_TableName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_Count);
+            this.Controls.Add(this.rtb_out);
+            this.Controls.Add(this.B_GenRandomInsert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cb_c8);
+            this.Controls.Add(this.cb_c7);
+            this.Controls.Add(this.cb_c6);
+            this.Controls.Add(this.cb_c5);
+            this.Controls.Add(this.cb_c4);
+            this.Controls.Add(this.cb_c3);
+            this.Controls.Add(this.cb_c2);
+            this.Controls.Add(this.cb_c1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_c8);
+            this.Controls.Add(this.tb_c7);
+            this.Controls.Add(this.tb_c6);
+            this.Controls.Add(this.tb_c5);
+            this.Controls.Add(this.tb_c4);
+            this.Controls.Add(this.tb_c3);
+            this.Controls.Add(this.tb_c2);
+            this.Controls.Add(this.tb_c1);
+            this.Name = "MainForm";
+            this.Text = "SQL Tools";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox tb_c1;
+        private TextBox tb_c2;
+        private TextBox tb_c3;
+        private TextBox tb_c4;
+        private TextBox tb_c7;
+        private TextBox tb_c6;
+        private TextBox tb_c5;
+        private TextBox tb_c8;
+        private Label label1;
+        private ComboBox cb_c1;
+        private ComboBox cb_c2;
+        private ComboBox cb_c3;
+        private ComboBox cb_c4;
+        private ComboBox cb_c8;
+        private ComboBox cb_c7;
+        private ComboBox cb_c6;
+        private ComboBox cb_c5;
+        private Label label2;
+        private Button B_GenRandomInsert;
+        private RichTextBox rtb_out;
+        private TextBox tb_Count;
+        private Label label3;
+        private Label label4;
+        private TextBox tb_TableName;
+        private Button B_Copy;
+        private Button B_Clear;
+        private Button B_InsertIntoTable;
+        private Label label5;
+        private TextBox tb_Database;
+        private Label label6;
+        private TextBox tb_Server;
+        private Button B_CurrencyRate;
+    }
+}
---
+++ MSSQL-Tools/MainForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using flangoLib;
+
+namespace MSSQL_Tools
+{
+    public partial class MainForm : Form
+    {
+        //readonly List<string> columns = new();
+        readonly List<ComboBox> cbs = new();
+        public MainForm()
+        {
+            InitializeComponent();
+            Initialize();
+
+            //columns = MakeColumns();
+
+            FLib.Print("--= Thank you for using flangoLib! =--");
+
+            rtb_out.Text = "\n\n\n\t\t\t--= Welcome to SQL Tools by flangopink! =--\n\t\t\t\t--= Powered by flangoLib =--";
+        }
+
+        private void B_GenRandomInsert_Click(object sender, EventArgs e)
+        {
+            List<string> selectedTypes = new();
+            foreach (ComboBox cb in cbs)
+                selectedTypes.Add(cb.Text);
+            
+            rtb_out.Text = FLib.GenerateInsertQuery(selectedTypes, int.Parse(tb_Count.Text), tb_TableName.Text);
+            B_InsertIntoTable.Enabled = true;
+        }
+
+        /*List<string> MakeColumns()
+        {
+            return new List<string>() { tb_c1.Text, tb_c2.Text, tb_c3.Text, tb_c4.Text, tb_c5.Text, tb_c6.Text, tb_c7.Text, tb_c8.Text };
+        }*/
+
+        void Initialize()
+        {
+            cbs.Add(cb_c1); cbs.Add(cb_c2); cbs.Add(cb_c3); cbs.Add(cb_c4);
+            cbs.Add(cb_c5); cbs.Add(cb_c6); cbs.Add(cb_c7); cbs.Add(cb_c8);
+
+            foreach (ComboBox cb in cbs)
+                foreach (string t in FLib.ColumnTypes)
+                    cb.Items.Add(t);
+        }
+
+        private void B_Copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(rtb_out.Text);
+        }
+
+        private void B_Clear_Click(object sender, EventArgs e)
+        {
+            rtb_out.Text = "";
+            B_InsertIntoTable.Enabled = false;
+        }
+
+        private void B_InsertIntoTable_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show($"This will insert {tb_Count.Text} row(s) into a *REAL* SQL database. Continue?", "Insert into SQL database.", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                string connetionString = $"Data Source={tb_Server.Text};Initial Catalog={tb_Database.Text};Integrated Security=SSPI;Encrypt=True;TrustServerCertificate=True;";
+                SqlConnection connection;
+                SqlCommand command;
+                string sql = rtb_out.Text;
+
+                connection = new SqlConnection(connetionString);
+                try
+                {
+                    connection.Open();
+                    command = new SqlCommand(sql, connection);
+                    command.ExecuteNonQuery();
+                    command.Dispose();
+                    connection.Close();
+                    MessageBox.Show("Success!");
+                    rtb_out.Text = "";
+                    B_InsertIntoTable.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Can't open connection!\n\n"+ex);
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                return;
+            }
+        }
+
+        private void B_CurrencyRate_Click(object sender, EventArgs e)
+        {
+            //rtb_out.Text = FLib.GetCurrencyRates().Keys;
+        }
+    }
+}
